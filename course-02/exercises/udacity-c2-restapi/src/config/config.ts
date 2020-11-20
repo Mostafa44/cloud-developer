6e4dc656bb6888764be549d5990cdd3a6dec2747
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2020-11-20 22:13:52 +0200

    use the environement variables instead of the config

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,19 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.POSTGRES_USERNAME,
+    password: "Cairo_1234",
+    database: "udagramyossefdev",
+    host: "udagramyoussefdev.cnglbuygvkqz.eu-west-3.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "eu-west-3",
+    aws_profile: "default",
+    aws_media_bucket: "udagram-yossef-dev",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
--- course-02/exercises/udacity-c2-restapi/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/src/sequelize.ts
@@ -1,17 +1,19 @@
-import {Sequelize} from 'sequelize-typescript';
-import { config } from './config/config';
-
-
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+require('dotenv').config();
 const c = config.dev;
+console.log(c);
+//console.log(process.env)
 
+console.log(process.env)
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
-  "username": c.username,
-  "password": c.password,
+  "username": `${process.env.POSTGRES_USERNAME}`,
+  "password": `${process.env.POSTGRES_PASSWORD}`,
   "database": c.database,
-  "host":     c.host,
+  "host": c.host,
 
-  dialect: 'postgres',
-  storage: ':memory:',
+
+  dialect: "postgres",
+  storage: ":memory:",
 });
-
