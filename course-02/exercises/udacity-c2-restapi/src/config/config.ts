a11563dba74ccadf35939fd92e538eb8bd179d38
mostafa.abdelshafy <mostafa.abdelshafy88@gmail.com>
2020-12-04 15:32:20 +0200

    use enevironment variables instead of config variables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,15 +1,17 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 export const config = {
   dev: {
     username: process.env.POSTGRES_USERNAME,
-    password: "Cairo_1234",
+    password: process.env.POSTGRES_PASSWORD,
     database: "udagramyossefdev",
     host: "udagramyoussefdev.cnglbuygvkqz.eu-west-3.rds.amazonaws.com",
     dialect: "postgres",
     aws_region: "eu-west-3",
-    aws_profile: "default",
+    aws_profile: process.env.AWS_PROFILE,
     aws_media_bucket: "udagram-yossef-dev",
     jwt: {
-      "secret": "helloworld"
+      "secret": process.env.JWT_SECRET
     }
   },
   prod: {
